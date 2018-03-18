7ce14cc3fb9af6be6f32ed11fe0634f30da5376d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-18 01:40:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,30 @@ class ApplicationController < Sinatra::Base
   get '/' do
 
   end
+  get '/posts/new'
+
+  erb :new
+  end
+  post '/posts'
+Post.create(params)
+redirect to '/posts'
+
+end
+get '/posts'
+@posts= Post.all
+erb :index
+end
+get '/posts/:id'
+Post.find_by_id(params[:id])
+erb :show
+end
+get '/posts/:id/edit'
+Post.find_by_id(params[:id])
+
+erb :edit
+end
+delete '/posts/:id/delete'
+erb :delete
+end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  @@all= []
+  def initialize(args)
+    @name=args[:name]
+    @content=args[:content]
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
